d6e3ee6bf23596cc9750468121ef70a4aad6c2b8
Headbwoi <headbwoi25@gmail.com>
2023-07-14 18:08:47 +0100

    add header to vr

--- vr_page/tailwind.config.js
+++ vr_page/tailwind.config.js
@@ -1,16 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1111px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        orb: ["var(--font-orb)"],
+        fell: ["var(--font-fell)"],
+      },
+      colors: {
+        background: "#010208",
       },
     },
   },
